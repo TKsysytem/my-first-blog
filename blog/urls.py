87f533af9b30e1afe2a9e0b60c1c73679fda92e1
Tsujimoto Kouji <k-tsujimoto@bu.k-cloud70.biz>
2020-08-06 11:21:04 +0900

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,18 @@
+""" Djangoの path 関数と、blog アプリの全ての ビューをインポート
+"""
+
+from django.urls import path
+from . import views
+
+"""
+post_list という名前のビューをルートURLに割り当てる
+このパターンは誰かがあなたのWebサイトの 'http://127.0.0.1:8000/' というアドレスにアクセスしてきたらviews.post_list
+が正しい行き先だということをDjangoに伝える。
+
+name='post_list' は、ビューを識別するために使われるURL の名前
+
+"""
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
