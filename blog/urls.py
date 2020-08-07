21389b1e8c4cd770054add9ccba4d653bf5ad3b9
Tsujimoto Kouji <k-tsujimoto@bu.k-cloud70.biz>
2020-08-07 11:45:34 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -16,5 +16,7 @@ name='post_list' は、ビューを識別するために使われるURL の名�
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 
